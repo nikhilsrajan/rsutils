39856bb06bb207d4ca573e77d598c9f636fac954
Nikhil Sasi Rajan <nikhilsasirajan@gmail.com>
2024-09-03 13:59:27 +0200

    added get_grid_geometry_from_latlon

--- s2_grid_utils.py
+++ s2_grid_utils.py
@@ -3,33 +3,43 @@ import shapely.geometry
 import shapely.affinity
 import pandas as pd
 import geopandas as gpd
+import s2cell
+import numpy as np
+
+
+# source: https://s2geometry.io/resources/s2cell_statistics
+RES_TO_KM_RANGE = {
+    0: (7842, 7842),
+    1: (3921, 5004),
+    2: (1825, 2489),
+    3: (840, 1167),
+    4: (432, 609),
+    5: (210, 298),
+    6: (108, 151),
+    7: (54, 76),
+    8: (27, 38),
+    9: (14, 19),
+    10: (7, 9),
+    11: (3, 5),
+    12: (1699 / 1000, 2),
+    13: (850 / 1000, 1185 / 1000),
+    14: (425 / 1000, 593 / 1000),
+    15: (212 / 1000, 296 / 1000),
+}
 
 
 def grid_size_2_res_mapping(grid_size_km:int=5):
-    # source: https://s2geometry.io/resources/s2cell_statistics
-    """
-    based on grid_size provided by user - return resolution value
-    :param grid_size:
-    """
-    if grid_size_km > 20 or grid_size_km < 1:
-        raise NotImplementedError(
-            f"grid size (km): {grid_size_km} not supported by our workflows at the moment"
-            f"please provide anything b/w 1 to 20 both inclusive"
-        )
-
-    if 14 <= grid_size_km <= 20:
-        return 9
-    elif 7 <= grid_size_km <= 10:
-        return 10
-    elif 3 <= grid_size_km <= 5:
-        return 11
-    elif grid_size_km == 1:
-        return 13
-    else:
-        raise NotImplementedError(
-            f"grid size (km): {grid_size_km} not supported by our workflows at the moment"
-            f"please provide anything from (1, 2, 5, 10)"
-        )
+    km_list= []
+    res_list = []
+    for res, km_range in RES_TO_KM_RANGE.items():
+        km_list.append(km_range[0])
+        km_list.append(km_range[1])
+        res_list.append(res)
+        res_list.append(res)
+    
+    km_list = np.array(km_list)
+    km_diff = np.abs(km_list - grid_size_km)
+    return res_list[np.argmin(km_diff)]
     
 
 def get_s2_grids_gdf(
@@ -66,7 +76,7 @@ def get_s2_grids_gdf(
     return s2_grids_gdf
 
 
-def get_grid_geometry(grid_id, scale_fact:float=1.1):
+def get_grid_geometry_from_id(grid_id, scale_fact:float=1.1):
     """
     Returns a shapely polygon for a given grid_id
     """
@@ -75,3 +85,36 @@ def get_grid_geometry(grid_id, scale_fact:float=1.1):
     ))
     geometry = shapely.affinity.scale(geometry, xfact=scale_fact, yfact=scale_fact)
     return geometry
+
+
+def get_id_from_latlon(lat:float, lon:float, grid_size_km:float=5, res:int=None,):
+    if res is None:
+        res = grid_size_2_res_mapping(grid_size_km=grid_size_km)
+
+    grid_id = s2cell.lat_lon_to_token(
+        lat = lat,
+        lon = lon,
+        level = res,
+    )
+
+    return grid_id
+
+
+def get_grid_geometry_from_latlon(
+    lat:float, 
+    lon:float,
+    grid_size_km:float=5,
+    res:int = None,
+    scale_fact:float=1.1,
+):
+    grid_id = get_id_from_latlon(
+        lat = lat,
+        lon = lon,
+        grid_size_km = grid_size_km,
+        res = res,
+    )
+
+    return get_grid_geometry_from_id(
+        grid_id = grid_id,
+        scale_fact = scale_fact,
+    )
